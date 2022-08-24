c8f0f0199893b8c5b926868dd7c199bd5c53086a
Diegoalejandroingelec <ramirezvargasdiegoalejandro2@gmail.com>
2022-08-24 16:41:26 -0400

    improve performance

--- RISLEY_PRISM.py
+++ RISLEY_PRISM.py
@@ -12,7 +12,7 @@ import cv2
 from scipy.io import loadmat
 from scipy.signal import savgol_filter
 import time
-
+from numpy import ndarray
 def risley_optical_index_fused_silica(l):
     n=np.sqrt((((0.6961663)*l**2)/(l**2-0.0684043**2))+((0.4079426*l**2)/(l**2-0.1162414**2))+((0.8974794*l**2)/(l**2-9.896161**2))+1)
     return n
@@ -26,7 +26,10 @@ def generate_2D_pattern(tf,
                         a,
                         number_of_prisms,
                         n_prism,
-                        expected_dims):
+                        expected_dims,
+                        plot_masks):
+    
+
     #Number Of laser pulses in image capture time
     num_pulse=tf*PRF
     #laser spot number
@@ -41,11 +44,7 @@ def generate_2D_pattern(tf,
     tr3=2*np.pi*w3*t1
     #Angle of risley 4
     tr4=2*np.pi*w4*t1
-    
-    
-    
-    
-    
+
     n11=np.array([list(-np.cos(tr1)*np.sin(a)),
                   list(-np.sin(tr1)*np.sin(a)),
                   list(np.cos(a)*np.ones(tr1.shape[0]))])
@@ -53,9 +52,7 @@ def generate_2D_pattern(tf,
     n12=np.array([0*tr1,
                   0*tr1,
                   np.ones(tr1.shape[0])])
-    n21=np.array([0*tr2,
-                  0*tr2,
-                  np.ones(tr2.shape[0])])
+    n21=n12
     n22=np.array([list(np.cos(tr2)*np.sin(a)),
                   list(np.sin(tr2)*np.sin(a)),
                   list(np.cos(a)*np.ones(tr2.shape[0]))])
@@ -63,13 +60,9 @@ def generate_2D_pattern(tf,
     n31=np.array([list(-np.cos(tr3)*np.sin(a)),
                   list(-np.sin(tr3)*np.sin(a)),
                   list(np.cos(a)*np.ones(tr3.shape[0]))])
-    n32=np.array([0*tr3,
-                  0*tr3,
-                  np.ones(tr3.shape[0])])
+    n32=n12
     
-    n41=np.array([0*tr4,
-                  0*tr4,
-                  np.ones(tr4.shape[0])])
+    n41=n12
     n42=np.array([list(np.cos(tr4)*np.sin(a)),
                   list(np.sin(tr4)*np.sin(a)),
                   list(np.cos(a)*np.ones(tr4.shape[0]))])
@@ -80,17 +73,17 @@ def generate_2D_pattern(tf,
     
     dot_product=np.dot(beam_a[-1],N[1])
     
-    
+
+
     for j in range(number_of_prisms*2):
         if((j+1)%2==0):
-            dot_product=np.array([np.dot(beam_a[-1][:,index],N[j][:,index]) for index in range(N[j].shape[1])])
-            
+            #dot_product=np.array([np.dot(beam_a[-1][:,index],N[j][:,index]) for index in range(N[j].shape[1])])
+            dot_product=np.einsum('ij,ji->i', np.transpose(beam_a[-1]),N[j])
             escalar_part=(np.sqrt(1-(n_prism)**2*(1-(dot_product)**2))-((n_prism))*dot_product)
             vector_of_escalar=np.array([escalar_part,
                                         escalar_part,
                                         escalar_part])
             first_term_vec=(n_prism)*beam_a[-1]
-            
             beam_a_new=first_term_vec+vector_of_escalar*N[j]
         else:
             if(j==0):
@@ -103,8 +96,8 @@ def generate_2D_pattern(tf,
                 first_term_vec=np.transpose(first_term_vec)
                 beam_a_new=first_term_vec+vector_of_escalar*N[j]
             else:
-                dot_product=np.array([np.dot(beam_a[-1][:,index],N[j][:,index]) for index in range(N[j].shape[1])])
-                
+                #dot_product=np.array([np.dot(beam_a[-1][:,index],N[j][:,index]) for index in range(N[j].shape[1])])
+                dot_product=np.einsum('ij,ji->i', np.transpose(beam_a[-1]),N[j])
                 escalar_part=(np.sqrt(1-(1/n_prism)**2*(1-(dot_product)**2))-((1/n_prism))*dot_product)
                 vector_of_escalar=np.array([escalar_part,
                                             escalar_part,
@@ -113,7 +106,9 @@ def generate_2D_pattern(tf,
                 
                 beam_a_new=first_term_vec+vector_of_escalar*N[j]
         beam_a.append(beam_a_new)
-        
+       
+   
+    
     # plt.rcParams["figure.figsize"] = (20,20)
     # plt.plot(beam_a[4][1],beam_a[4][0],'.')
     # plt.title('PATTERN USING 2 PRISMS')
@@ -126,7 +121,8 @@ def generate_2D_pattern(tf,
         
  
             
-      
+    
+    
     x_min=np.min(beam_a[-1][1])
     y_min=np.min(beam_a[-1][0])
     x_factor=np.abs((expected_dims[1]/2)/x_min)
@@ -137,24 +133,44 @@ def generate_2D_pattern(tf,
     
     x = x+np.abs(np.min(x))
     y = y+np.abs(np.min(y))-27.5
-    # plt.rcParams["figure.figsize"] = (10,10)
-    # plt.plot(x,y,'.')  
-    # plt.plot([0,0,1000,1000,0],[100,0,0,100,100],'r')
-    # plt.title('PATTERN USING 4 PRISMS')
-    # plt.show()  
+    if(plot_masks):
+        plt.rcParams["figure.figsize"] = (10,20)
+        plt.plot(x,y,'.')  
+        plt.plot([0,0,1000,1000,0],[100,0,0,100,100],'r')
+        plt.title('PATTERN USING 4 PRISMS')
+        plt.show()  
+    
+    
     risley_pattern_2D=np.zeros((expected_dims[1],expected_dims[2]))
-    for i in range(int(num_pulse)):
-        discrete_points_x=round(x[i]) if (round(x[i])>0 and round(x[i])<expected_dims[1]) else expected_dims[1]-1 if round(x[i])>=expected_dims[1] else 0
-        discrete_points_y=round(y[i]) if (round(y[i])>0 and round(y[i])<expected_dims[2]) else expected_dims[2]-1 if round(y[i])>=expected_dims[2] else 0
-        
-        risley_pattern_2D[discrete_points_x,discrete_points_y]=1
-        
+    
+    aa=ndarray.round(x)
+    bb=ndarray.round(y)
+    
+
+    # aa=[xx if (xx > 0 and xx < expected_dims[1]) else expected_dims[1]-1 if xx >= expected_dims[1] else 0 for xx in aa]
+    # bb=[yy if (yy > 0 and yy < expected_dims[2]) else expected_dims[2]-1 if yy >= expected_dims[2] else 0 for yy in bb]
+    keep_x_coordinate=np.logical_and(aa >= 0 , aa < expected_dims[1])
+    keep_y_coordinate=np.logical_and(bb >= 0 , bb < expected_dims[2])
+    
+    remove_coordinates=np.logical_not(np.logical_and(keep_x_coordinate,keep_y_coordinate))
+    bb=[d for (d, remove) in zip(bb, remove_coordinates) if not remove]
+    aa=[d for (d, remove) in zip(aa, remove_coordinates) if not remove]
+
+
+    coordinates=np.array((np.array(aa).astype(int),np.array(bb).astype(int)))
+    
+    
+    
+    risley_pattern_2D[tuple(coordinates)] = 1
+    
+    
     transmittance=(risley_pattern_2D.sum()*100)/(expected_dims[1]*expected_dims[2])
     
     # cv2.imshow('Risley_pattern_2D',risley_pattern_2D.astype(np.uint8)*255)
     # cv2.waitKey(0)
     # cv2.destroyAllWindows()
-    print('TRANSMTTANCE C-SCAN',transmittance)
+    #print('TRANSMTTANCE C-SCAN',transmittance)
+    
     return risley_pattern_2D,transmittance
 
 
@@ -170,14 +186,8 @@ def get_transmittances(path):
         data = loadmat(path)
         oct_volume = data['images']
         return oct_volume
-    
-    
-    
-
 
     original_volume=read_data(path)
-
-
     mean_b_scans=np.mean(original_volume,2)
     mean_b_scans=mean_b_scans[30:,:].astype(np.uint8)
     
@@ -187,15 +197,11 @@ def get_transmittances(path):
     total_mean=np.mean(means)
 
     x=np.linspace(0,511,512)
-    transmittances=gaussian(x, 0.48, total_mean, 100)
-    plt.plot(x,transmittances)
-    plt.show()
-    return transmittances*100
-    # plt.imshow(mean_b_scans,cmap='gray')
-    # plt.plot(means_smooth,label='means')
-    # plt.title('Average of B-scans')
-    # plt.legend()
+    transmittances=gaussian(x, 0.53, total_mean, 100)
+    # plt.plot(x,transmittances)
     # plt.show()
+    return transmittances*100
+
 
 def create_risley_pattern(expected_dims,
                           line_width,
@@ -208,7 +214,8 @@ def create_risley_pattern(expected_dims,
                           w4,
                           a,
                           number_of_prisms,
-                          volume_path):
+                          volume_path,
+                          plot_masks):
     
     mask_risley=np.zeros(expected_dims)
     transmittance_list=[]
@@ -228,7 +235,8 @@ def create_risley_pattern(expected_dims,
                                 a,
                                 number_of_prisms,
                                 n_prism,
-                                expected_dims) 
+                                expected_dims,
+                                plot_masks) 
             transmittance_list.append(transmittance)
             mask_risley[i-1,:,:]=mask_2D
     else:
@@ -253,13 +261,12 @@ def create_risley_pattern(expected_dims,
             else:
                 new_transmittances[index_of_maximum+indx]=minimum_transmittance-(count_down*0.01)
                 count_down+=1
-                
-        # plt.plot(new_transmittances) 
-        # plt.show()
+        print('EXPECTED FINAL TRANSMITTANCE: ', new_transmittances.sum()/expected_dims[0])
+        if(plot_masks):    
+            plt.plot(new_transmittances) 
+            plt.show()
         required_prfs=required_prf(np.array(new_transmittances))
         for i in range(expected_dims[0]):
-            print(i)
-            
             #Risley optical index fused silica
             n_prism=risley_optical_index_fused_silica((i*line_width+start_wavelength)/1000)
             #print('Risley optical index fused silica ',n)
@@ -272,10 +279,10 @@ def create_risley_pattern(expected_dims,
                                 a,
                                 number_of_prisms,
                                 n_prism,
-                                expected_dims) 
+                                expected_dims,
+                                plot_masks) 
             transmittance_list.append(transmittance)
             mask_risley[i,:,:]=mask_2D
-        
         
     print('MEAN TRANSMTTANCE C-SCAN',np.mean(transmittance_list))
     transmittance_list=[]
@@ -325,7 +332,7 @@ start_wavelength=962
 
     
 path='../oct_original_volumes/AMD/Farsiu_Ophthalmology_2013_AMD_Subject_1084.mat'
-start = time.process_time()
+begin = time.time()
 mask_risley=create_risley_pattern(expected_dims,
                           line_width,
                           start_wavelength,
@@ -337,9 +344,10 @@ mask_risley=create_risley_pattern(expected_dims,
                           w4,
                           a,
                           number_of_prisms,
-                          volume_path=path)
-
-print('TIME ELAPSED FOR GENERATING RISLEY MASK:', time.process_time() - start, 's')
+                          volume_path=path,
+                          plot_masks=False)
+end = time.time()
+print(f"TIME ELAPSED FOR GENERATING RISLEY MASK: {end - begin}")
 # x=np.linspace(1999000,7119000,513)
 # x=512320.7283068506
 # y=4.256303756050208+1.13743618e-05*x+-5.00772883e-13*x**2
@@ -351,9 +359,9 @@ print('TIME ELAPSED FOR GENERATING RISLEY MASK:', time.process_time() - start, '
 
 
 # from skimage import data
-import napari
+# import napari
 
-viewer = napari.view_image(mask_risley*255)
+# viewer = napari.view_image(mask_risley*255)
 
 # img=mask
 # f = np.fft.fft2(img.astype(np.float32))
