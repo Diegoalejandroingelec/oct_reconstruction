f71285953c548342f5e8ebc537c3ebb6269fecc3
Diegoalejandroingelec <ramirezvargasdiegoalejandro2@gmail.com>
2022-09-19 16:40:11 -0400

    fix

--- 3D_autoencoder_pytorch/reconstructions_with_motion.py
+++ 3D_autoencoder_pytorch/reconstructions_with_motion.py
@@ -473,13 +473,15 @@ def evaluate_model(denoised_dataset_folder_path,
             if(compare_with_roi):
                 print('ROI COMPARISON')
                 window_for_comparison, upper_limit, lower_limit=get_window_for_comparison(original_volume,window_size=original_volume_dim,comparison_size=comparison_size)
+                print(window_for_comparison.shape)
                 if(denoised_ground_truth_for_comparison):
+                    print(denoised_original_volume.shape)
                     volume_for_comparison=np.multiply(denoised_original_volume,window_for_comparison).astype(np.uint8)
                     original_volume=denoised_original_volume[:,:,11:89]
                 else:
                     volume_for_comparison=np.multiply(original_volume,window_for_comparison).astype(np.uint8)
                 
-                reconstruction_for_comparison=np.multiply(bigger_reconstruction,window_for_comparison[:,:,11:89]).astype(np.uint8)
+                reconstruction_for_comparison=np.multiply(bigger_reconstruction,window_for_comparison).astype(np.uint8)
                 print('evaluate metrics on roi')
                 PSNR=compute_PSNR(volume_for_comparison,reconstruction_for_comparison)
                 RMSE=compute_RMSE(volume_for_comparison,reconstruction_for_comparison)
